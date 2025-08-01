67906fc541cc9181ea62f7a679e25b849ccfbeb9
mediaprophet <34872169+mediaprophet@users.noreply.github.com>
2025-08-01 12:37:48 +1000

    minor updates

--- n3logic-vscode-extension/n3logic/builtins/N3LogicBuiltins.ts
+++
@@ -1,9 +0,0 @@
-
-// Modularized builtins re-export
-export { MathBuiltins } from './N3LogicMathBuiltins';
-export { StringBuiltins } from './N3LogicStringBuiltins';
-export { ListBuiltins } from './N3LogicListBuiltins';
-export { TimeBuiltins } from './N3LogicTimeBuiltins';
-export { LogicBuiltins } from './N3LogicLogicBuiltins';
-export { TypeBuiltins } from './N3LogicTypeBuiltins';
-export { OtherBuiltins } from './N3LogicOtherBuiltins';
